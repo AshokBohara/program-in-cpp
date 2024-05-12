8b6d7b929785cf9343ef71a15cd9579323f82ffa
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-05-12 22:27:33 +0545

    Add files via upload
    using c++ to write hello world

---
+++ hello world in c++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Print a message to the console
+    cout << "Hello, world!" << endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
+
