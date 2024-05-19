5139b24b45f55e456ecb98d8d05e88e8d705e408
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-05-19 20:24:33 +0545

    Update square from 1 to 10.cpp

--- square from 1 to 10.cpp
+++ square from 1 to 10.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    for(int i = 1; i <= 10; ++i) {
+int main() 
+{
+    for(int i = 1; i <= 10; ++i) //using for loop to print square of number one to ten 
+    {
         int square = i * i;
         // Print the number and its square
         cout << "Square of " << i << " is: " << square << endl;
