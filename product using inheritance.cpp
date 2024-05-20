232677910e792864b18f14287ff7e95fa3d6b85e
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-05-20 22:49:53 +0545

    Add files via upload

---
+++ product using inheritance.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class number
+{
+  protected:
+    int n1, n2;
+  public:
+    void input(int a, int b)
+    {
+      n1 = a;
+      n2 = b;
+    }	
+};
+
+class product : public number
+{
+  public:
+    void display()
+    {
+      cout << "product = " << n1 * n2 << endl;
+    }
+};
+
+int main()
+{
+  product p; // Correctly declare an object of type product
+  p.input(5, 4); // Call the input method on the object p
+  p.display(); // Call the display method on the object p
+  return 0;
+}
+
