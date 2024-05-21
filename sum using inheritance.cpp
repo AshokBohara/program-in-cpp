d89c0f21ec6f966072573204be6f37c695be5d48
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-05-21 19:45:29 +0545

    Add files via upload

---
+++ sum using inheritance.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class number
+{
+	protected:
+		int n1,n2;
+		public:
+		void input(int a, int b)
+		{
+		n1=a;
+		n2=b;
+			
+		}
+	
+};
+class sum:public number
+{
+	public:
+		void display()
+		{
+			cout<<"sum ="<<n1+n2<<endl;
+		}
+};
+int main()
+{
+	sum s;
+	s.input(4,5);
+	s.display();
+	return 0;
+}
