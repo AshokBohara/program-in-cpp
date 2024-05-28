1efef2d949ac5dce7e63946ea190e52dc638548b
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-05-28 20:51:21 +0545

    Add files via upload

---
+++ multilevel inheritance eg2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Number 
+{
+protected:
+    int num;
+public:
+    void setNumber(int n) 
+	{
+        num = n;
+    }
+    int getNumber() const 
+	{
+        return num;
+    }
+};
+
+// Derived class from Number
+class Arithmetic : public Number 
+{
+public:
+    int square() const 
+	{
+        return num * num;
+    }
+};
+
+// Derived class from Arithmetic
+class AdvancedArithmetic : public Arithmetic 
+{
+public:
+    int cube() const 
+	{
+        return num * num * num;
+    }
+};
+
+int main() 
+{
+    AdvancedArithmetic obj;
+    obj.setNumber(3);
+    
+    cout << "Number: " << obj.getNumber() << endl;
+    cout << "Square: " << obj.square() << endl;
+    cout << "Cube: " << obj.cube() << endl;
+    
+    return 0;
+}
+
