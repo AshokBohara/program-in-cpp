a1b982842a9026d5cf85ba9ee93cbbe23c11cc62
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-06-02 21:48:13 +0545

    Update check Evenodd using parameter constructor.cpp

--- check Evenodd using parameter constructor.cpp
+++ check Evenodd using parameter constructor.cpp
@@ -1,6 +1,6 @@
+//checking entered number is odd or even using c++ language
 #include <iostream>
 using namespace std;
-
 class oddeven {
     private:
         int n;
@@ -18,10 +18,8 @@ class oddeven {
             }
         }
 };
-
 int main() {
     oddeven o(2); // Creating an object of the oddeven class with n = 2
     o.display();  // Calling the display method to check if the number is odd or even
     return 0;
 }
-
