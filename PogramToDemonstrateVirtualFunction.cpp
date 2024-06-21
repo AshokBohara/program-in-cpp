3f897c5338ebfbca480035b27d231588ecaaf796
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-06-21 21:37:06 +0545

    Add files via upload

---
+++ PogramToDemonstrateVirtualFunction.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class base {
+public:
+    void display() {
+        cout << "base display" << endl;
+    }
+    virtual void show() {
+        cout << "base show" << endl;
+    }
+};
+
+class derived : public base {
+public:
+    void display() {
+        cout << "derived display" << endl;
+    }
+    void show() override {
+        cout << "derived show" << endl;
+    }
+};
+
+int main() {
+    base *bptr;
+    base b;
+    derived d;
+    bptr = &b;
+    bptr->display(); 
+    bptr->show();    
+    bptr = &d;
+    bptr->display(); // Calls base class display
+    bptr->show();    // Calls derived class show due to virtual function
+
+    return 0;
+}
+
