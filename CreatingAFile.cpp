1f06b7f139afbaa0e94abcbd1b0ef66e3e6fb68b
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-06-30 22:20:31 +0545

    Add files via upload

---
+++ CreatingAFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	fstream file;
+	file.open("firstfile.txt",ios::in);
+    string x;
+    while(getline(file,x))
+    cout<<x<<endl;
+	file.close();
+	return(0);
+	
+}
