7d74d8d86caa489faf6e86e3746848615d14813f
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-07-02 23:03:00 +0545

    Add files via upload

--- CreatingAFile.cpp
+++ CreatingAFile.cpp
@@ -1,15 +1,16 @@
-//creating a file.txt in cpp 
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-int main()
-{
-	fstream file;
-	file.open("firstfile.txt",ios::in);
-    string x;
-    while(getline(file,x))
-    cout<<x<<endl;
-	file.close();
-	return(0);
-	
+
+int main() {
+    fstream file;
+    // Corrected the file path to use double backslashes or a single forward slash
+    file.open("c:\\firstfile.txt", ios::out);
+    
+    // Check if the file is open before writing to it
+    file << "hello BSE 2nd sem" << endl;
+    file.close();
+
+    return 0;
 }
+
