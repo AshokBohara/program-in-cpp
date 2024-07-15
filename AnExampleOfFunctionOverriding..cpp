212eb7c85e0d87006320d4ef4de90145624be62b
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-07-15 20:31:27 +0545

    Add files via upload

---
+++ AnExampleOfFunctionOverriding..cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class base{
+	public:
+		void show()
+		{
+			cout<<"Test Class  has been invoked."<<endl;
+			
+		}
+	
+};
+class derived : public base{
+	public:
+		void show()
+		{
+			cout<<"Derived class has been invoked.";
+		}
+};
+int main()
+{
+	derived obj;
+	obj.show(); //function of derived class.
+	obj.base::show(); //function of base class.
+	return 0;
+}
