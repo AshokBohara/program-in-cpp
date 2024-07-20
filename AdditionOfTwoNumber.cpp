e7c40a40605add34a08409adc42160f4c24ef4a2
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-07-20 19:40:52 +0545

    Update AdditionOfTwoNumber.cpp

--- AdditionOfTwoNumber.cpp
+++ AdditionOfTwoNumber.cpp
@@ -1,24 +1,19 @@
 //adding two number using parameterized constructor
 #include <iostream>
-
 using namespace std;
-
 class Ashok {
 private:
     int a, b, sm;
-
 public:
     Ashok(int a, int b) {
         this->a = a;
         this->b = b;
     }
-
     void adNum() {
         sm = a + b;
         cout << sm;
     }
 };
-
 int main() {
     Ashok p1(3, 4);
     p1.adNum();
