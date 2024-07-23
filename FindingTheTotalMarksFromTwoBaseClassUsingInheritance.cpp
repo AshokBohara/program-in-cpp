aa077e857aed57acd06560dde0f8b63fa54fdfc4
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-07-23 20:47:45 +0545

    Add files via upload

---
+++ FindingTheTotalMarksFromTwoBaseClassUsingInheritance.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class Student{
+	protected:
+		int roll;
+		public:
+			void fun1()
+			{
+			
+			cout<<"Enter the roll number"<<endl;
+			cin>>roll;
+		    }
+};
+class Test : protected Student{
+    protected:
+	float sub1,sub2;
+	public:
+		void initialize(){
+			cout<<"Enter the marks in 2 subjects respectively"<<endl;
+			cin>>sub1>>sub2;
+		}
+};
+class Result : protected Test{
+	float total;
+	public:
+		void display()
+		{
+			fun1();
+			initialize();
+			cout<<"The total marks is"<<sub1+sub2<<endl;
+		}
+};
+int main()
+{
+	Result obj;
+	obj.display();
+	return 0;
+}
