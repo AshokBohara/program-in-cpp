ee2440d6956c06921a9a7668c92535fb18c8c4a4
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-07-27 20:42:35 +0545

    Add files via upload

---
+++ AddingComplexNumbersUsingConstructors.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class add_complex{
+	int real;
+	float imaginary;
+	public:
+		add_complex(int a, float b){
+			real=a;
+			imaginary=b;
+		}
+		void add(add_complex t1, add_complex t2)
+		{
+			real=t1.real+t2.real;
+			imaginary=t1.imaginary+t2.imaginary;
+		
+		}
+		void display()
+		{
+			cout<<"The required real number is"<<real<<"whereas imaginary is"<<imaginary;
+		}
+};
+
+int main()
+{
+	add_complex t1(3,4.5);
+	add_complex t2(5,7.89);
+	add_complex t3;
+	t3.add(t1,t2);
+	t3.display();
+	
+	return 0;
+}
+	
