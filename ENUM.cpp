0774eb4f48e172ca02726a27bff7c12370f31f9c
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-08-25 21:21:15 +0545

    Add files via upload

---
+++ ENUM.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+enum week{sun,mon,tue,wed,thur,fri,sat};
+int main()
+{
+    char selected_day;
+    printf("enter day(1-7)\n");
+    scanf("%d",&selected_day);
+    if(selected_day==sun)
+    {
+        printf("selected day is sunday");
+
+    }
+    else 
+	if(selected_day==mon)
+    {
+        printf("selected day is monday");
+    }
+    else
+    if(selected_day==tue)
+    {
+        printf("selected day is tuesday");
+    }
+}
