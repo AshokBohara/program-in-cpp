438757216f9fd3796d2fb88b9b0f1cadb91f06f0
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-10-01 20:33:11 +0545

    Add files via upload

---
+++ FindingAgeOfTree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Tree 
+{
+private:
+    int rings; // Number of rings in the tree
+
+public:
+    // Constructor to initialize the tree's rings
+    Tree(int r) : rings(r) {}
+
+    // Function to calculate and display the age of the tree
+    void displayAge() 
+	{
+        if (rings > 0) 
+		{
+            cout << "The tree is approximately " << rings << " years old." << endl;
+        } else {
+            cout << "Invalid number of rings. The tree age can't be determined." << endl;
+        }
+    }
+};
+
+int main() 
+{
+    int rings;
+
+    // Input: number of rings in the tree
+    cout << "Enter the number of rings in the tree: ";
+    cin >> rings;
+
+    // Create Tree object and display its age
+    Tree tree(rings);
+    tree.displayAge();
+
+    return 0;
+}
+
