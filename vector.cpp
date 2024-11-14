1dae1c2546b656e6a4f567de48cbf103ac2ad85b
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-11-14 07:45:35 +0545

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+//vector
+
+// int main()
+// {
+//     vector<int>v(1,4); // [4]
+//     v.push_back(1); // [4, 1]
+//     v.emplace_back(2); // [4, 1, 2]
+//     return 0;
+// }
+
+//iterator.
+
+int main()
+{
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(8);
+    v.push_back(11);
+    vector<int>::iterator it=v.begin();
+    // vector<string> vec; 
+    // vec.push_back("ashok");
+    // vec.push_back("bohara");
+    // vector<string>::iterator itor= vec.begin();
+    cout<<*++(++it);//display 11 ..
+    return(0);
+}
