779da21169ebadd4dccc400972867facac66c315
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-11-16 14:25:53 +0545

    Add files via upload

---
+++ exampleOfRecursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int sumOfN(int n)
+{
+    if(n==0)
+        return 0;
+    return n+sumOfN(n-1);
+}
+int factorial( int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+int main()
+{
+    int n=5;
+      cout<<sumOfN(n);
+    cout<<endl;
+    cout<<factorial(n);
+    return 0;
+    return 0;
+}
