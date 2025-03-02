f855af3f29da2f85047d0fd2e770033b0c578236
Ashok Bohara <boharaashok32@gmail.com>
2025-03-02 20:09:20 +0545

    modified

--- DSA_Learn/towerOfHanoi.cpp
+++ DSA_Learn/towerOfHanoi.cpp
@@ -1,24 +1,22 @@
-//Tower Of Hanoi using c++
+// implementation of tower of hanoi using c++..
 #include <iostream>
 using namespace std;
 
-void towerOFhanoi(int n, char source, char dest, char aux)
-{
-  if(n == 1)
-  {
-    cout<<"  Move DISK "<<n<<" from "<<source<<" to "<<dest<<endl;
-    return;
-  }
-  towerOFhanoi(n-1, source, aux, dest);
-  cout<<"  Move DISK "<<n<<" from "<<source<<" to "<<dest<<endl;
-  towerOFhanoi(n-1, aux, dest, source);
+void towerOfHanoi(int n, char source, char dest, char aux) {
+    if (n == 1) {
+        cout << "Move DISK " << n << " from " << source << " to " << dest << endl;
+        return; // Exit the function after moving the disk
+    }
+
+    towerOfHanoi(n - 1, source, aux, dest);
+    cout << "Move DISK " << n << " from " << source << " to " << dest << endl;
+    towerOfHanoi(n - 1, aux, dest, source);
 }
 
-int main()
-{
+int main() {
     int n;
-    cout<<"\n\n Enter number of disks: ";
-    cin>>n;
-    towerOFhanoi(n, 'S', 'D', 'A');
+    cout << "Enter number of disks: ";
+    cin >> n;
+    towerOfHanoi(n, 'S', 'D', 'A'); // S = Source, D = Destination, A = Auxiliary
     return 0;
 }
